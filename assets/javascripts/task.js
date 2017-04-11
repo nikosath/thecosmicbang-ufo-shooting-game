260e0dd6f4b3069f96ed5cc4dc394b49d4afc689
Nikos Athanasakis <emidekol@gmail.com>
2017-04-11 21:41:05 +0300

    feat: Completed task without jQuery & Bootstrap

--- assets/javascripts/task.js
+++ assets/javascripts/task.js
@@ -1,12 +1,81 @@
-// $(document).ready(startGame);
-window.onload = startGame;
+// $(document).ready(shootUFOGame.start());
+window.onload = shootUFOGame;
 
-function startGame() {
-  var gameMap = document.getElementById('game-map');
-  var countdownTimer, movementTimer;
-  var ship = document.getElementById('spaceship');
-  var rightBound = gameMap.offsetWidth - ship.offsetWidth;
-  var bottomBound = gameMap.offsetHeight - ship.offsetHeight;
+function shootUFOGame() {
+  var elmGameMap = document.getElementById('game-map');
+  // The setInterval() timer that's defined inside initialiseCountdown()
+  var countdownTimer;
+  // The setTimeout() timer that's defined inside scheduleMove()
+  var movementTimer;
+  // increaseScore() is defined inside initialiseScore()
+  var increaseScore;
+
+  var elmShip = document.getElementById('spaceship');
+  elmShip.onclick = function handleShipClick() {
+    increaseScore();
+    hideShip();
+  };
+  
+  // Rightmost bound the ship can move to
+  var rightBound = elmGameMap.offsetWidth - elmShip.offsetWidth;
+  // Bottommost bound the ship can move to
+  var bottomBound = elmGameMap.offsetHeight - elmShip.offsetHeight;
+
+  var elmCountdown = document.getElementById('countdown');
+  // Game duration
+  var secondsLeft = 10;
+
+  function initialiseCountdown() {
+    elmCountdown.innerHTML = 'Seconds left: ' + secondsLeft;
+    countdownTimer = setInterval(decreaseCountdown, 1000);
+  }
+
+  function decreaseCountdown() {
+    elmCountdown.innerHTML = 'Seconds left: ' + (secondsLeft -= 1);
+    // Game end condition
+    if (secondsLeft === 0) {
+      stopGame();
+    }
+  }
+
+  var elmScore = document.getElementById('score');
+  var score = 0;
+
+  function initialiseScore() {
+    elmScore.innerHTML = 'Score: ' + score;
+  }
+
+  increaseScore = function () {
+    elmScore.innerHTML = 'Score: ' + (score += 1);
+  };
+
+  function hideShip() {
+    elmShip.className = 'hidden';
+  }
+
+  function unhideShip() {
+    elmShip.className = '';
+  }
+
+  function moveShip() {
+    elmShip.style.left = Math.random() * rightBound + 'px';
+    elmShip.style.top = Math.random() * bottomBound + 'px';
+  }
+
+  function scheduleMove() {
+    var delay = Math.random() * 2000;
+    movementTimer = setTimeout(function () {
+      moveShip();
+      unhideShip();
+      scheduleMove();
+    }, delay);
+  }
+
+  function startGame() {
+    initialiseCountdown();
+    initialiseScore();
+    scheduleMove();
+  }
 
   function stopGame() {
     clearInterval(countdownTimer);
@@ -14,56 +83,5 @@ function startGame() {
     ship.onclick = null;
   }
 
-  function initialiseCountdown() {
-    var elem = document.getElementById('countdown');
-    var secondsLeft = 10;
-    elem.innerHTML = 'Seconds left: ' + secondsLeft;
-
-    countdownTimer = setInterval(decreaseCountdown, 1000);
-    function decreaseCountdown() {
-      elem.innerHTML = 'Seconds left: ' + (secondsLeft -= 1);
-      // Game end condition
-      if (secondsLeft === 0) {
-        stopGame();
-      }
-    }
-  }
-
-  // Make increaseScore() available to handleShipClick()
-  var increaseScore;
-
-  function initialiseScore() {
-    var elem = document.getElementById('score');
-    var score = 0;
-    elem.innerHTML = 'Score: ' + score;
-
-    increaseScore = function () {
-      elem.innerHTML = 'Score: ' + (score += 1);
-    };
-  }
-
-  ship.onclick = function handleShipClick() {
-    clearTimeout(movementTimer);
-    increaseScore();
-    moveShip();
-    scheduleMove();
-  };
-
-  function moveShip() {
-    ship.style.left = Math.random() * rightBound + 'px';
-    ship.style.top = Math.random() * bottomBound + 'px';
-  }
-
-  function scheduleMove() {
-    var delay = Math.random() * 2000;
-    // var delay = 2000;
-    movementTimer = setTimeout(function () {
-      moveShip();
-      scheduleMove();
-    }, delay);
-  }
-
-  initialiseCountdown();
-  initialiseScore();
-  scheduleMove();
+  startGame();
 }
